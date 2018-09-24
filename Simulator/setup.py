84414bde9610e4d32469c4071431fca6714acf6f
Connor Jops <jops@ucla.edu>
2018-09-24 02:40:09 -0700

    Make setup.py work under Mac OS X

--- Simulator/setup.py
+++ Simulator/setup.py
@@ -3,7 +3,8 @@ from numpy.distutils.misc_util import get_numpy_include_dirs
 
 module1 = Extension('simulator',
                     include_dirs = get_numpy_include_dirs(),
-                    sources = ['py.cpp', 'simulator.cpp'])
+                    sources = ['py.cpp', 'simulator.cpp'],
+                    extra_compile_args=['-std=c++11'])
 
 setup (name = 'simulator',
        version = '1.0',
